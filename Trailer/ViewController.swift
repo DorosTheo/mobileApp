60320df9573a06df4afb9491920017657e115a11
O'Brien Alaribe <obrienalaribe@gmail.com>
2015-11-24 14:31:12 +0000

    initial

---
+++ Trailer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trailer
+//
+//  Created by Doros Theodorou [sc13dt] on 24/11/2015.
+//  Copyright (c) 2015 Movie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
